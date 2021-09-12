e1dc7c2b30907c788abcc8048e3a2fc8aa5b7a1c
Sreeram Shenai <sreeramshenai2000@gmail.com>
2021-09-13 01:17:48 +0530

    basic build

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const billAmount = document.querySelector("#BillAmount")
 const cashAmount = document.querySelector("#CashAmount")
-console.log(billAmount.value)
+const checkButton = document.querySelector("#CheckButton")
+const cashDeno = ['2000','500','100','20','10','1']
+const notes = document.querySelectorAll(".notes-calculate")
+
+checkButton.addEventListener("click",function() {
+    let cashReq = cashAmount.value - billAmount.value
+    for(let i=0;i<cashDeno.length;i++) {
+        const notesReq = Math.trunc(cashReq/cashDeno[i])
+        cashReq = cashReq%cashDeno[i]
+        if(notesReq == 0) {
+            notes[i].innerText = ""
+        } else {
+            notes[i].innerText = notesReq
+        }
+        
+    }   
+})
